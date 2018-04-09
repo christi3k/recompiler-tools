c02bd244581dac3f62c358a87db5d97011f15cac
Christie Koehler <ck@christi3k.net>
2018-04-09 11:31:43 -0700

    Check for existence of URL and output text list.

--- notes.py
+++ notes.py
@@ -2,16 +2,18 @@ from bs4 import BeautifulSoup
 
 soup_out = BeautifulSoup('<ul id="show notes"></ul>', "lxml")
 
-with open("test.nhsx") as fp:
+with open("ep55.nhsx") as fp:
     soup_in = BeautifulSoup(fp, "xml")
 
 markers = soup_in.find_all('Marker')
 
 ulist = soup_out.ul
 
+text_output = "\n\n"
+
 print("now loop through markers")
 for m in markers:
-    if 'Type' in m.attrs and m['Type'] == 'Chapter':
+    if 'Type' in m.attrs and m['Type'] == 'Chapter' and 'URL' in m.attrs:
         new_tag = soup_out.new_tag("a", href=m['URL'])
         ulist.append(new_tag)
         new_tag.string = m['Name']
@@ -19,5 +21,8 @@ for m in markers:
         new_li.string = "["+m['Time'][:-4]+"] "
         new_tag.wrap(new_li)
 
+        text_output += "- ["+m['Time'][:-4]+"] " + m['Name'] + ": " + m['URL'] + "\n"
+
 print(ulist)
+print(text_output)
 #print(soup_out.prettify())
